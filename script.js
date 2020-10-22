720a0e4a54451b652472b54a9f9b9c6996332af4
oruizramos <70069043+oruizramos@users.noreply.github.com>
2020-10-22 23:20:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
+
+let result = 0
+let currentTime = timeLeft.textContent
+
+function randomSquare() {
+  square.forEach(className => {
+    className.classList.remove('mole')
+  })
+  let randomPosition = square[Math.floor(Math.random() * 9)]
+  randomPosition.classList.add('mole')
+
+  //Assigning id of the randomPosition to hitPosition later usage
+  hitPosition = randomPosition.id
+}
+
+square.forEach(id => {
+  id.addEventListener('mouseup', () => {
+    if(id.id === hitPosition){
+      result = result + 1
+      score.textContent = result
+      hitPosition=null
+    }
+  })
+})
+
+
+function moveMole() {
+  let timerId = null
+  timerId = setInterval(randomSquare, 500)
+}
+
+moveMole()
+
+
+function countDown() {
+  currentTime--
+  timeLeft.textContent = currentTime
+
+  if(currentTime === 0 ) {
+    clearInterval(timerId)
+    alert('It´s over! You smashed ' + result + ' objects')
+  }
+}
+
+let timerId = setInterval(countDown, 1000)
